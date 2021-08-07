ea59a59b655def32a2918c88c4a73cc9b086b2f8
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-08-07 00:01:57 -0400

    changed delete text to remove

--- src/components/shopping-cart/cart.jsx
+++ src/components/shopping-cart/cart.jsx
@@ -21,7 +21,7 @@ export default function Cart({ onAddToCart, onDelete }) {
                             <p>{item.description}</p>
                             <p>${item.price}</p>
                             <img src={item.image} alt="item" style={{width: '200px', height: '200px'}} />
-                            <button className='btn btn-danger btn-sm' onClick={() => onDelete(item.id)}>Delete</button>
+                            <button className='btn btn-danger btn-sm' onClick={() => onDelete(item.id)}>Remove</button>
                         </div>
                     ); 
                 })
